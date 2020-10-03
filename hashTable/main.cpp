92ddc9aa49cce23547f98267bc2ba13127e05ac4
m1cron <m1cron4900@gmail.com>
2020-10-03 04:53:48 +0300

    hash table main updated

--- hashTable/main.cpp
+++ hashTable/main.cpp
@@ -1,6 +1,5 @@
 #include "HashTable.cpp"
 
-
 int main(int argc, char **argv) {
     auto *table0 = new HashTable<std::string, std::string>(2);
     table0->insertItem("123", "code");
@@ -11,9 +10,9 @@ int main(int argc, char **argv) {
     std::cout << "\n\n";
 
     auto *table1 = new HashTable<std::string, std::string>(5);
-    table1->insertItem("46572356423465726587123465734126587346587", "ОАО Далбоеб");
-    table1->insertItem("98734579867394856734576983457698458769345", "ОАО Пидарас");
-    table1->insertItem("94650879089083465908746598746590879465874", "ОАО Хуесос");
+    table1->insertItem("46572356423465726587123465734126587346587", "ОАО PROVERKA");
+    table1->insertItem("98734579867394856734576983457698458769345", "ОАО Test");
+    table1->insertItem("94650879089083465908746598746590879465874", "ОАО Hahaha");
     table1->display();
     delete table1;
 
